c182d430dfd9e7689c4a3ff31061dc6f59272470
DempseyCLuMaye <dempseylumaye@u.boisestate.edu>
2024-09-20 15:20:51 -0600

    Movement&Camera
    Player 1 and 2 Movement and Jumping and Meh Camera Smoothing

---
+++ MultiplayerGame-Fall24/Assets/Scripts/Player1Movement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class MovementP1 : MonoBehaviour
+{
+    public float speed = 3f;
+    public float gravity = -20f;
+
+    private CharacterController characterController;
+    private Vector3 velocity; 
+
+    private bool isGrounded;
+    public float jumpForce = 5f; 
+
+    void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        isGrounded = characterController.isGrounded;
+        
+        float moveInput = Input.GetAxis("Player1Move");
+        Vector3 move = new Vector3(moveInput * speed, 0, 0);
+
+        
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = 0; 
+        }
+
+        if (Input.GetButtonDown("Player1Jump") && isGrounded)
+        {
+            velocity.y += jumpForce; 
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        characterController.Move((move + velocity) * Time.deltaTime);
+    }
+}
