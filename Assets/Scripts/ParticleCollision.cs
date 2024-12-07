4746bca2e69d96e82912d92092219fd5677c0536
StevenDrain <stevendrain@u.boisestate.edu>
2024-12-06 20:22:28 -0700

    fixed death in molten level

--- Assets/Scripts/ParticleCollision.cs
+++ Assets/Scripts/ParticleCollision.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class ParticleCollision : MonoBehaviour
 {
-    // Start is called before the first frame update
     public ParticleSystem part;
+    public GameObject respawnPoint;
+    public CharacterController Player1characterController;
+    public CharacterController Player2characterController;
 
     void Start()
     {
@@ -17,11 +19,40 @@ public class ParticleCollision : MonoBehaviour
         if (other.tag == "PlayerOne")
         {
             Debug.Log("Death1");
-
+            ResetLevelPlayer1();
         }
         else if (other.tag == "PlayerTwo")
         {
             Debug.Log("Death2");
+            ResetLevelPlayer2();
+        }
+    }
+
+    void ResetLevelPlayer1()
+    {
+        if (respawnPoint != null)
+        {
+            Player1characterController.enabled = false;
+            Player1characterController.transform.position = respawnPoint.transform.position;
+            Player1characterController.enabled = true;
+        }
+        else
+        {
+            Debug.LogError("Respawn point is not assigned.");
+        }
+    }
+
+    void ResetLevelPlayer2()
+    {
+        if (respawnPoint != null)
+        {
+            Player2characterController.enabled = false;
+            Player2characterController.transform.position = respawnPoint.transform.position;
+            Player2characterController.enabled = true;
+        }
+        else
+        {
+            Debug.LogError("Respawn point is not assigned.");
         }
     }
 }
